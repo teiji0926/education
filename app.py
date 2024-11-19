7310bc836fc4e059ff3d7b648966354e830cf235
teiji0926 <118594404+teiji0926@users.noreply.github.com>
2024-11-19 14:38:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,113 +12,124 @@ RETRY_DELAY = 5  # リトライ間隔（秒）
 st.sidebar.title("アプリ選択")
 app_selection = st.sidebar.radio("アプリを選択してください", ["キャリアカウンセラーアプリ", "教育提案アプリ"])
 
-# キャリアカウンセラーアプリ
+col1, col2, col3 = st.columns([1, 2, 1])  # 中央列を少し広めに設定
+
+
+
+
 if app_selection == "キャリアカウンセラーアプリ":
     st.title("キャリアカウンセラーアプリ")
-
+    with col2:
+        st.image(
+        "https://th.bing.com/th/id/OIP.Y23nBpZxgajNoKec58O0twHaHa?w=202&h=202&c=7&r=0&o=5&pid=1.7",  
+        use_column_width=True)
+        
     # Lambda 関数のエンドポイント URL
     counselor_url = 'https://pg2galxz0c.execute-api.ap-northeast-1.amazonaws.com/stage1/'
 
-    # 履歴を保持するためのセッションステート
+    # 会話履歴を保持するためのセッションステート
     if 'conversation_history' not in st.session_state:
         st.session_state['conversation_history'] = []
-    if 'response_ready' not in st.session_state:
-        st.session_state['response_ready'] = False
 
-    # キャリア相談の入力
+    # これまでの会話履歴を表示
     st.write("### 会話履歴")
     for chat in st.session_state['conversation_history']:
         with st.chat_message(chat["role"]):
             st.write(chat["content"])
 
-    career_question = st.text_input('キャリアに関する相談を入力してください:', '')
+    # ユーザーの質問を入力
+    career_question = st.text_input('キャリアに関する相談を入力してください:', 'ここを消して入力：例）5年後もなくならない仕事ができるようになりたい')
 
     # 相談するボタン
     if st.button('相談する'):
         if career_question:
-            # ユーザーの質問を履歴に追加
+            # ユーザーの入力を履歴に追加
             st.session_state['conversation_history'].append({"role": "user", "content": career_question})
-            st.session_state['response_ready'] = True  # フラグを立てる
 
-    # AIの応答を処理
-    if st.session_state['response_ready']:
-        with st.spinner('相談内容を処理中です...'):
-            try:
-                # Lambda関数にリクエスト送信
-                response = requests.post(
-                    url=counselor_url,
-                    json={
-                        "conversation_history": st.session_state['conversation_history'],  # 履歴全体を送信
-                        "user_input": career_question  # 今回の質問
-                    },
-                    headers={"Content-Type": "application/json"}
-                )
+            # AIの応答を処理
+            with st.spinner('相談内容を処理中です...'):
+                try:
+                    # Lambda 関数にリクエストを送信
+                    response = requests.post(
+                        url=counselor_url,
+                        json={
+                            "conversation_history": st.session_state['conversation_history'],
+                            "user_input": career_question
+                        },
+                        headers={"Content-Type": "application/json"}
+                    )
 
-                if response.status_code == 200:
-                    result = response.json()
+                    if response.status_code == 200:
+                        # Lambdaからの応答を取得
+                        result = response.json()
 
-                    # AIの応答を履歴に追加
-                    st.session_state['conversation_history'].append({"role": "assistant", "content": result["response"]})
-                else:
-                    st.error(f"エラー: ステータスコード {response.status_code}")
+                        # AIの応答を履歴に追加
+                        st.session_state['conversation_history'].append({"role": "assistant", "content": result["response"]})
+                    else:
+                        st.error(f"エラー: ステータスコード {response.status_code}")
 
-            except Exception as e:
-                st.error(f"リクエストエラー: {e}")
-            finally:
-                st.session_state['response_ready'] = False  # フラグをリセット
+                except Exception as e:
+                    st.error(f"リクエストエラー: {e}")
 
 # 教育提案アプリ
 elif app_selection == "教育提案アプリ":
-    st.title("教育提案アプリ")
-    st.text('検索には30秒くらいかかります。気長にお待ちください。')
-    st.text('503エラーの時は再検索してください')
+    st.title("教育提案アプリ　LinkedInとAidemyから研修を検索してきます")
+    with col2:
+        st.image(
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi8vyJho9t80345AD_apyUqcEZ_buav7VArC19VWGSX3j_xeKrs3J1pAYjHqKKP_gfsctyC3uya943aZK53aqBnxlb-yHo-Np1CcxFK6Drzwd0q3uvOU4MgHuwvLOiVy7vmP2JkRRBLfm1g/s800/searchbox14.png",  
+        use_column_width=True)
+        st.text('検索には30秒くらいかかります。気長にお待ちください。')
+        st.text('503エラーの時は再検索してください')
 
     # APIエンドポイントのURL
     education_url = 'https://53u1zlkx3h.execute-api.ap-northeast-1.amazonaws.com/stage1/education_test'
 
+
+
     # キャリア目標の入力
-    career_goal = st.text_input('キャリア目標を入力してください:', 'AIエンジニア')
+    career_goal = st.text_input('キャリア目標を入力してください:', 'ここを消して入力：例）AIエンジニア')
 
     # コースを推薦するボタン
     if st.button('コースを推薦する'):
         if career_goal:
-            for attempt in range(MAX_RETRIES):
-                try:
-                    # APIリクエストを送信
-                    response = requests.get(education_url, params={'career_goal': career_goal})
+            with st.spinner('検索中...しばらくお待ちください。'):
+                for attempt in range(MAX_RETRIES):
+                    try:
+                        # APIリクエストを送信
+                        response = requests.get(education_url, params={'career_goal': career_goal})
 
-                    # ステータスコードを表示
-                    st.write(f"ステータスコード: {response.status_code}")
-                    st.write(response.text)
+                        # ステータスコードを表示
+                        st.write(f"ステータスコード: {response.status_code}")
+                        st.write(response.text)
 
-                    # ステータスコードが200（成功）の場合
-                    if response.status_code == 200:
-                        # '''json''' タグを削除してリストの形式だけを表示する
-                        cleaned_text = re.sub(r"```json|```", "", response.text.strip())
+                        # ステータスコードが200（成功）の場合
+                        if response.status_code == 200:
+                            # '''json''' タグを削除してリストの形式だけを表示する
+                            cleaned_text = re.sub(r"json|", "", response.text.strip())
 
-                        # 各コース情報を個別に表示
-                        courses = re.findall(r"{.*?}", cleaned_text)  # 各コース情報を抽出
-                        for course_str in courses:
-                            course_info = eval(course_str)  # 文字列を辞書に変換
+                            # 各コース情報を個別に表示
+                            courses = re.findall(r"{.*?}", cleaned_text)  # 各コース情報を抽出
+                            for course_str in courses:
+                                course_info = eval(course_str)  # 文字列を辞書に変換
 
-                            # 各項目を太字で大きめのサイズで強調表示
-                            st.markdown(f"<span style='font-size:20px; font-weight:bold;'>提供元: {course_info.get('提供元', '不明')}</span>", unsafe_allow_html=True)
-                            st.markdown(f"<span style='font-size:18px; font-weight:bold;'>コース名: {course_info.get('コース名', '不明')}</span>", unsafe_allow_html=True)
-                            st.markdown(f"<span style='font-size:16px;'>内容: {course_info.get('内容', '不明')}</span>", unsafe_allow_html=True)
-                            st.markdown("<hr>", unsafe_allow_html=True)  # 区切り線
-                        break  # 成功したらループを抜ける
+                                # 各項目を太字で大きめのサイズで強調表示
+                                st.markdown(f"<span style='font-size:20px; font-weight:bold;'>提供元: {course_info.get('提供元', '不明')}</span>", unsafe_allow_html=True)
+                                st.markdown(f"<span style='font-size:18px; font-weight:bold;'>コース名: {course_info.get('コース名', '不明')}</span>", unsafe_allow_html=True)
+                                st.markdown(f"<span style='font-size:16px;'>内容: {course_info.get('内容', '不明')}</span>", unsafe_allow_html=True)
+                                st.markdown("<hr>", unsafe_allow_html=True)  # 区切り線
+                            break  # 成功したらループを抜ける
 
-                    else:
-                        st.warning(f"エラー: ステータスコードが {response.status_code} です。リトライ {attempt + 1} 回目")
+                        else:
+                            st.warning(f"エラー: ステータスコードが {response.status_code} です。リトライ {attempt + 1} 回目")
+                            time.sleep(RETRY_DELAY)  # リトライ間隔
+
+                    except Exception as e:
+                        st.error(f"リクエストエラー: {e}")
                         time.sleep(RETRY_DELAY)  # リトライ間隔
 
-                except Exception as e:
-                    st.error(f"リクエストエラー: {e}")
-                    time.sleep(RETRY_DELAY)  # リトライ間隔
-
-            # 最大リトライ回数に達した場合のエラーメッセージ
-            else:
-                st.error("リトライしても503エラーが発生しました。後ほど再度お試しください。")
+                # 最大リトライ回数に達した場合のエラーメッセージ
+                else:
+                    st.error("リトライしても503エラーが発生しました。後ほど再度お試しください。")
 
         else:
             st.warning('キャリア目標を入力してください。')
